4aa571b7d0010996dea4f9f747217c534a7f8ce7
aditipds <aditiprasad345@gmail.com>
2025-07-28 16:02:55 +0530

    a

---
+++ Desktop/mad2/config.py
@@ -0,0 +1,8 @@
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = '23f1001093@ds.study.iitm.ac.in'
+MAIL_PASSWORD = 'jrbnfgdgpgwpjnlf'
+MAIL_DEFAULT_SENDER = '23f1001093@ds.study.iitm.ac.in'
